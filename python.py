e3eaac84827e1a7a56a593d52deac7ed56ef2973
Raghavendra Jingade <jraghavendra842@gmail.com>
2023-09-15 22:41:21 +0530

    Update python.py

--- python.py
+++ python.py
@@ -97,3 +97,200 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+# Web Scraper using BeautifulSoup and Requests:
+# This Python code snippet scrapes data from a website using the BeautifulSoup library to parse HTML and the Requests library to fetch web pages.
+
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://example.com'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Extract and print some data from the webpage
+title = soup.title.string
+print(f'Title: {title}')
+
+# Find and print all links on the webpage
+links = soup.find_all('a')
+for link in links:
+    print(link.get('href'))
+    
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
+Object-Oriented Programming (OOP):
+An example of a Python class that represents a simple bank account.
+
+    
+class BankAccount:
+    def __init__(self, account_number, balance=0):
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            print('Insufficient funds')
+
+    def get_balance(self):
+        return self.balance
+
+account1 = BankAccount('12345')
+account1.deposit(1000)
+account1.withdraw(500)
+print(f'Account Balance: {account1.get_balance()}')
+
+
+# Data Analysis with Pandas:
+# An example of using the Pandas library for data analysis.
+
+
+import pandas as pd
+
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+        'Age': [25, 30, 35],
+        'City': ['New York', 'San Francisco', 'Los Angeles']}
+
+
+
+
+# User
+# give me example DSA questions and solutions code in  python language
+# ChatGPT
+# Certainly! Here are some example data structures and algorithms (DSA) questions along with Python solutions:
+
+# 1. Reverse a String:
+# Write a Python function to reverse a string.
+
+def reverse_string(s):
+    return s[::-1]
+
+# Example usage
+input_str = "Hello, World!"
+reversed_str = reverse_string(input_str)
+print(reversed_str)
+
+
+# 2. Find the Maximum Element in an Array:
+# Write a Python function to find the maximum element in an array.
+
+
+def find_max_element(arr):
+    if len(arr) == 0:
+        return None
+    max_element = arr[0]
+    for num in arr:
+        if num > max_element:
+            max_element = num
+    return max_element
+
+# Example usage
+numbers = [5, 3, 9, 1, 7]
+max_num = find_max_element(numbers)
+print(f"The maximum element is {max_num}")
+
+
+# 3. Check for Palindrome:
+# Write a Python function to check if a given string is a palindrome.
+
+
+def is_palindrome(s):
+    s = s.lower().replace(" ", "")
+    return s == s[::-1]
+
+# Example usage
+test_str = "A man a plan a canal Panama"
+is_palindrome_result = is_palindrome(test_str)
+print(f'Is it a palindrome? {is_palindrome_result}')
+
+# 4. Implement Binary Search:
+# Write a Python function to perform binary search on a sorted array.
+
+def binary_search(arr, target):
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+# Example usage
+sorted_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+target_value = 5
+result_index = binary_search(sorted_array, target_value)
+print(f"Element {target_value} found at index {result_index}")
+
+
+# 5. Implement a Linked List:
+# Create a simple singly linked list in Python with basic operations like insertion and deletion.
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+
+# Example usage
+linked_list = LinkedList()
+linked_list.append(1)
+linked_list.append(2)
+linked_list.append(3)
+linked_list.display()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
